b40a6bf6b43c567369de134e337249ef71696fed
Fernando <fernando_lopezc@live.com>
2019-03-13 05:03:52 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,5 +1,5 @@
 def count_elements(array)
-  counts = Hash.new(0) #default value is 0
-  array.each { |name| counts[name]+=1}
+  counts = Hash.new(0) # default value is 0
+  array.each { |name| counts[name] += 1 }
   counts
 end
